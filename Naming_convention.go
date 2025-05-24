d8c857a9f028232194e7c12c19018fe5391d8ab0
Jackhammer-xD <197812319+Jackhammer-xD@users.noreply.github.com>
2025-05-24 15:34:54 +0530

    Hello to Go

---
+++ Naming_convention.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	//pascal case >>> use for naming types of go
+	//eg: calculateareas , userinfo , NewHTTPRequest
+	//Structs, interfaces , enums
+
+	//snake_case
+	//words that are written in lower case sepratted by space
+	//eg:- user_id , frist_name , http_request
+
+	//UPPERCASE
+	//use case is Constants
+
+	//Mixedcase
+	//eg: javaScript, htmlDocument , isValid ( small letter at starting)
+
+}
